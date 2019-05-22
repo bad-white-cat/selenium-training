9ebd8c63de3d1cb7d8a0b5e45da0208249f6b831
Olga Patapava <arabezzz@gmail.com>
2019-05-22 22:40:42 +0300

    initial test

---
+++ index.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const {Builder, Key, By, until} = require('selenium-webdriver');
+
+describe('Checkout Google.com', function () {
+    let driver;
+
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    it('Search on Google', async function() {
+        await driver.get('https://google.com');
+        await driver.findElement(By.name('q')).click();
+        await driver.findElement(By.name('q')).sendKeys('olya', Key.RETURN);
+        await driver.wait(until.elementLocated(By.id('rcnt')), 10000);
+
+        let title = await driver.getTitle();
+        expect(title).to.equal('olya - Поиск в Google');
+    });
+
+    after(() => driver.quit());
+})
