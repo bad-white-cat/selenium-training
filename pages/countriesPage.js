3198e9176d1732ba62d821cc9a0657c0e06c4cb8
Olga Patapava <gangstawhitecat@gmail.com>
2019-06-03 23:36:55 +0300

    homework9 preparations

--- pages/countriesPage.js
+++ pages/countriesPage.js
@@ -7,13 +7,16 @@ class countriesPage extends adminPage {
   }
 
   getCountriesRows() {
-    return this.driver.findElements(By.xpath('//form[@name="countries_form"]//tr[contains(@class,"row")]'));
+    return this.driver.findElements(By.css('form[name="countries_form"] tr.row'));
   }
 
   getCountriesList() {
     return this.driver.findElements(By.xpath('//form[@name="countries_form"]//tr[contains(@class,"row")]//a[not(@title)]'));
   }
 
+  getZoneNumber(el = this.driver) {
+    return el.findElement(By.xpath('.//td[5]'));
+  }
 }
 
 module.exports = countriesPage;
--- specs/countries-spec.js
+++ specs/countries-spec.js
@@ -1,4 +1,4 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 const page = require('../pages/countriesPage.js');
 
 const chai = require("chai"),
@@ -14,6 +14,10 @@ describe('Countries page:', function () {
     Page = await new page(driver);
   });
 
+  /*Homework #9
+  Сделайте сценарии, которые проверяют сортировку стран и геозон (штатов) в учебном приложении litecart.
+    1) на странице http://localhost/litecart/admin/?app=countries&doc=countries
+    а) проверить, что страны расположены в алфавитном порядке */
   it('countries should be in alphabet order', async function() {
     await Page.loginToAdmin();
     await Page.navigateToPage('Countries');
@@ -21,6 +25,49 @@ describe('Countries page:', function () {
     const countriesList = await Promise.all(countriesElements.map(country => country.getText()));
 
     expect(countriesList).to.be.sorted();
+  });
+
+  /*Homework #9
+    1) на странице http://localhost/litecart/admin/?app=countries&doc=countries
+    б) для тех стран, у которых количество зон отлично от нуля -- открыть страницу этой страны и там проверить, что зоны расположены в алфавитном порядке
+  */
+  it.only('geo zones should be in alphabet order', async function() {
+    await Page.loginToAdmin();
+    await Page.navigateToPage('Countries');
+    const countriesRows = await Page.getCountriesRows();
+    
+    /*for (let row of countriesRows) {
+      const zone = await row.findElement(By.xpath('.//td[6]'));
+      const number = await zone.getText();
+      console.log(number);
+    }*/
+
+    const filterCountries = async country => {
+      const zone = await country.findElement(By.xpath('.//td[6]'));
+      const number = await zone.getText();
+      return number > 0;
+    }
+
+    const countriesToCheck = await Promise.all(countriesRows.filter(filterCountries));
+    console.log(countriesToCheck.length);
+
+    /*const list = [] //...an array filled with values
+
+const functionWithPromise = item => { //a function that returns a promise
+  return Promise.resolve('ok')
+}
+
+const anAsyncFunction = async item => {
+  return await functionWithPromise(item)
+}
+
+const getData = async () => {
+  return await Promise.all(list.map(item => anAsyncFunction(item)))
+}
+
+const data = getData()
+console.log(data)
+*/
   });
 
   after(() => driver.quit());
